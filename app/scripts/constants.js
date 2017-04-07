ce8a99f4f7ec85a93e2791561dcdb5f3e1acf545
Chris Chase <chris@chasenc.com>
2017-04-07 09:39:33 -0400

    Use catalog demo fork. Enable catalog.

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -89,7 +89,7 @@ angular.extend(window.OPENSHIFT_CONSTANTS, {
   ],
 
   ENABLE_TECH_PREVIEW_FEATURE: {
-    service_catalog_landing_page: false,
+    service_catalog_landing_page: true,
     pod_presets: false
   },
 
